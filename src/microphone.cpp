212a09d642be8480c4c13a6da21925f66598d190
jerinpeter <jerinpeter@gmail.com>
2020-12-30 20:18:26 +0530

    made sub, pub object global

--- src/microphone.cpp
+++ src/microphone.cpp
@@ -2,26 +2,26 @@
 #include "std_msgs/String.h"
 using namespace std;
 char chat[20];
+ros::Publisher my_pub;
+ros::Subscriber my_sub;
+
 void CallBack(const std_msgs::String::ConstPtr&msg)
 {
     cout<<msg->data.c_str();
-    cout<<"Enter a msg:";
+    cout<<"Message:";
     cin>>chat;
-   
-    //ROS_INFO("Got this from the speaker: [%s]",msg->data.c_str());
+    std_msgs::String mg;
+    mg.data =chat;
+    my_pub.publish(mg);
+    //ROS_INFO("%s",mg.data.c_str());
 
 }
-
 int main(int argc , char **argv)
 {
     ros::init(argc,argv,"microphone");
     ros::NodeHandle nh;
-    ros::Subscriber my_sub = nh.subscribe("tomic",1000,CallBack);
-    ros::NodeHandle n;
-    ros::Publisher my_pub = n.advertise<std_msgs::String>("tospeaker",10);
-    std_msgs::String mg;
-    mg.data =chat;
-    my_pub.publish(mg);
+    my_sub = nh.subscribe("tomic",10,CallBack);
+    my_pub = nh.advertise<std_msgs::String>("tospeaker",10);
     ros::spin();
     return 0;
 
--- src/speaker.cpp
+++ src/speaker.cpp
@@ -1,34 +1,35 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+
 char chat[20];
-std_msgs::String mg;
 
 using namespace std;
-void CallBack(const std_msgs::String::ConstPtr&msg)
+ros::Publisher my_pub;
+ros::Subscriber my_sub;
+
+void CallBack(std_msgs::String mg)
 {
-cout<<"Reply:";
-cout<<msg->data.c_str();
+//cout<<"Reply:";
+cout<<"Message:";
+    cin>>chat;
+      std_msgs:: String msg;
+      msg.data = chat;
+      //ROS_INFO("%s",msg.data.c_str());
+     // cout<<msg.data.c_str();
+      my_pub.publish(msg);
+      cout<<endl;
+cout<<"hello";
+ cout<<mg.data<<endl;
+ // ROS_INFO("I heard: [%s]", mg->data.c_str());
 }
 int main(int argc, char **argv)
 {
   ros::init(argc,argv,"speaker");
   ros::NodeHandle nh;
-  ros::Publisher my_pub = nh.advertise<std_msgs::String>("tomic",10);
-  ros::Subscriber my_sub = nh.subscribe("tospeaker",1000,CallBack);
-
-  ros::Rate loop_rate(10);
-
-  while(ros::ok())
-  {   
-    cout<<"MESSAGE:";
-    cin>>chat;
-      std_msgs:: String msg;
-      msg.data = chat;
-      ROS_INFO("%s",msg.data.c_str());
-      my_pub.publish(msg);
-      ros::spinOnce();
-      loop_rate.sleep();
-      cout<<endl;
-  } 
+   my_pub = nh.advertise<std_msgs::String>("tomic",10);   
+  cout<<"new msg";
+   my_sub = nh.subscribe("tospeaker",10,CallBack);
+   ros::spin();
+ 
 return 0;
 }
