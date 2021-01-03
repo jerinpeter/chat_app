b5b5b65b1bda7f0e1950557e41226a86fb061a3f
jerinpeter <jerinpeter@gmail.com>
2021-01-03 18:55:25 +0530

    commented the code

--- src/microphone.cpp
+++ src/microphone.cpp
@@ -1,32 +1,43 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+
 using namespace std;
-char chat[20];
-ros::Publisher my_pub;
-ros::Subscriber my_sub;
+char chat[20];          //variable for storing the user input
+
+ros::Publisher my_pub;        //creation of publisher object
+ros::Subscriber my_sub;       //creation of subscriber object
 
 void CallBack(const std_msgs::String::ConstPtr&msg)
 {
-    cout<<msg->data.c_str();
+    cout<<msg->data.c_str();        //Prints the msg from the speaker 
+    cout<<endl;
     cout<<"Message:";
     cin>>chat;
+    cout<<endl;                           
     std_msgs::String mg;
     mg.data =chat;
-    my_pub.publish(mg);
-    //ROS_INFO("%s",mg.data.c_str());
+    my_pub.publish(mg);             //Publishes the message to speaker
 
 }
 int main(int argc , char **argv)
 {
-    ros::init(argc,argv,"microphone");
-    ros::NodeHandle nh;
-
-    ros::AsyncSpinner spinner(2);
-    spinner.start();
+    ros::init(argc,argv,"microphone");  //initialization and naming the node micrphone
+     
+    ros::NodeHandle nh;   // creation of nodehandle object
+    ros::AsyncSpinner spinner(2);    // (2) implies uses 2 threads
+    spinner.start();                 
   
-    
     my_sub = nh.subscribe("tomic",10,CallBack);
+    /*
+    will subscribe to the topic /tomic with the  master and will call the
+    CallBack() whenever a new message is recieved
+    */
     my_pub = nh.advertise<std_msgs::String>("tospeaker",10);
+    /*
+    Tells the rosmaster that this node is going to publish a msg
+     of std_msgs/String type and will allow the master tell any nodes listening
+     to tospeaker that this node is going to publish a message 
+    */
 
     ros::waitForShutdown();
 
--- src/speaker.cpp
+++ src/speaker.cpp
@@ -1,43 +1,46 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-char chat[20];
+char chat[20];  //variable for storing the user input
+using namespace std;   //for using cout statement 
 
-using namespace std;
 ros::Publisher my_pub;
 ros::Subscriber my_sub;
 
 void CallBack(const std_msgs::String::ConstPtr& mg)
 {
-      ROS_INFO("I heard: [%s]", mg->data.c_str());
+      cout<<mg->data.c_str();     //prints the msg from microphone
+      cout<<endl;
 }
 int main(int argc, char **argv)
 {
   ros::init(argc,argv,"speaker");
-  ros::NodeHandle nh;
-
-  ros::AsyncSpinner spinner(2);
+  ros::NodeHandle nh;      //creation of nodehandle object
+  ros::AsyncSpinner spinner(2);  // creation of AsyncSpinner with 2 threads
   spinner.start();
-  
-  
-   my_pub = nh.advertise<std_msgs::String>("tomic",10);   
-  cout<<"new msg";
+
+   my_pub = nh.advertise<std_msgs::String>("tomic",10);
+   /*
+    Tells the rosmaster that this node is going to publish a msg
+     of std_msgs/String type and will allow the master tell any nodes listening
+     to tomic that this node is going to publish a message 
+    */   
    my_sub = nh.subscribe("tospeaker",10,CallBack);
-   while (ros::ok())
-   {
-    cout<<"Message:";
-    cin>>chat;
-    std_msgs:: String msg;
-    msg.data = chat;
-      //ROS_INFO("%s",msg.data.c_str());
-     // cout<<msg.data.c_str();
-    my_pub.publish(msg);
-    cout<<endl;
-    cout<<"hello";
-    cout<<msg.data<<endl;
-   
-   
-   //ros::spinOnce();
-   }
+   /*
+    will subscribe to the topic /tospeaker with the  master and will call the
+    CallBack() whenever a new message is recieved
+    */
+    while (ros::ok())
+    {
+      cout<<"Message:";
+      cin>>chat;
+      cout<<endl;
+      std_msgs:: String msg;
+      msg.data = chat; 
+      my_pub.publish(msg);       //publsing the msg
+      cout<<endl; 
+      cout<<msg.data<<endl;
+    }
+
 return 0;
 }
